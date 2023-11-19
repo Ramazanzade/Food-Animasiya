8a77bfaacead1b98a605eef3a6783be129320c2b
Nizam <ramazanov570633@gmail.com>
2023-11-19 23:00:59 +0400

    Mushroom

--- App.tsx
+++ App.tsx
@@ -1,16 +1,60 @@
-import { View, Text, Image } from 'react-native'
-import React from 'react'
+import { View, Text, Image, TouchableOpacity } from 'react-native'
+import React, { useState } from 'react'
 import appcss from './src/main/appcss'
 
 const App = () => {
+
+  const [clickCount, setClickCount] = useState(0);
+
+  const handlePress = () => {
+    if (clickCount <= 59) {
+      setClickCount(clickCount + 1);
+    }
+    console.log(`Clicked ${clickCount} times`);
+  }
+
+  const renderImages = () => {
+    const images = [];
+
+    for (let i = 0; i < clickCount; i++) {
+      function getRandomDegree() {
+        return Math.floor(Math.random() * 360);
+      }
+      const imageStyle = {
+        transform: [{ rotate: `${getRandomDegree()}deg` }],
+      };
+      images.push(
+        <View key={i} style={{width:10,height:10,margin:'5%' , display:'flex'}}>
+          <Image
+            source={require('./src/asset/Imge/Gobelek.png')}
+            style={[appcss.img2,imageStyle]}
+          />
+        </View>
+      );
+    }
+
+    return images;
+  }
+
   return (
     <View style={appcss.view}>
-      <View style={appcss.imgview}>
-        <Image
-          source={require('./src/asset/Imge/Pizza.png')}
-          style={appcss.img}
-        />
+      <View>
+        <View style={appcss.imgview}>
+          <Image
+            source={require('./src/asset/Imge/Pizza.png')}
+            style={appcss.img}
+          />
+        </View>
+        <View style={appcss.view2}>
+          {renderImages()}
+        </View>
       </View>
+      <TouchableOpacity style={appcss.touc} onPress={() => handlePress()} >
+        <Image
+          source={require('./src/asset/Imge/Gobelek.png')}
+          style={appcss.img1}
+        />
+      </TouchableOpacity>
     </View>
   )
 }
--- src/main/appcss.tsx
+++ src/main/appcss.tsx
@@ -7,6 +7,38 @@ export default StyleSheet.create({
 
     },
     imgview: {
+        alignSelf: 'center',
+        marginTop: '10%',
+        position:'relative',
+    
+    },
+    touc: {
+        // width: 100,
+        // height: 150,
+    },
+    img1: {
+        width: 150,
+        height: 150,
 
+    },
+    img2: {
+        width: 30,
+        height: 30,
+
+    },
+    view2: {
+        zIndex:2,
+        position:'absolute',
+        alignSelf:'center',
+        height:150,
+        borderRadius:150,
+        overflow: 'hidden',
+        display:'flex',
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        justifyContent: 'center',
+        alignItems: 'center',
+        width:220,
+        marginTop:'32%'
     }
 })
